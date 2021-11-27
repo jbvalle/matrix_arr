61cbad6243409fdfedd29005028d72ecfba59578
Ubuntu <ubuntu@ubuntu2110.linuxvmimages.local>
2021-11-27 14:48:08 -0500

    first stable version

--- others/str2args.h
+++
@@ -1,6 +0,0 @@
-#ifndef STR2ARGS_H
-#define STR2ARGS_H
-
-void ptr2args(char **args, char *input_string, int *args_num);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -4,6 +4,17 @@
 #include "str2args.h"
 #include "matavg.h"
 
+void add_args_to_matrix(char **args,float *matrix, int rows, int columns){
+    //Parse input string arguments to matrix_arr
+    //Convert to floating Type
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < columns; j++){
+          *(matrix + i * columns + j) = atof(args[i * columns + j]);
+
+      }
+    }
+}
+
 int main(void){
 
     //ptr2ptr to acces matrices with array indices
@@ -12,42 +23,51 @@ int main(void){
     char *input_string = (char*)malloc(100*sizeof(char));
 
     int  rows, columns;
-
     //Initialize input_string with terminator
     for(int i = 0; i < 100; i++)input_string[i] = '\0';
 
+    //Input - Dimensions & Matrix Values ----------------------------------
     //Read in dimensions
     scanf("%dx%d",&rows,&columns);getchar();
-
-    //Memory allocate the matrice
-    float *matrix = (float*)malloc(rows*columns*sizeof(float));
-    //Memory allocate averaged matrice
-    //Rows and columns reduces by 2
-    float *matrix_avrg = (float*)malloc((rows-2)*(columns-2)*sizeof(float));
-
-    //Letterwise input from stdin
+    //Read Metrix demensions
     char letter;
     for(int i = 0, j = 0; j < rows;j++){
       for(; (letter = getchar())!='\n';){
             input_string[i++] = letter;
       }input_string[i++] = ' ';
     }
+    //End: Input - Dimensions & Matrix Values ----------------------------------
+
+    //Allocate Memory for input and Output Matrix----------------------------
+    //Memory allocate the matrice
+    float *matrix = (float*)malloc(rows*columns*sizeof(float));
+    //Memory allocate averaged matrice
+    //Rows and columns reduces by 2
+    float *matrix_avrg = (float*)malloc((rows-2)*(columns-2)*sizeof(float));
+    //End: Allocate Memory for input and Output Matrix----------------------------
+
 
     //Parse input string to ptr array
     //Parameters: args - array | input stream | maximum number of arguments to be fetched
     ptr2args(args, input_string,rows*columns);
 
+    //Returns arguments values of input to matrix
+    add_args_to_matrix(args, matrix, rows, columns);
 
-    //Parse input string arguments to matrix_arr
-    printf("Input Matrix:\n");
-    for(int i = 0; i < rows; i++){
-      for(int j = 0; j < columns; j++){
-          *(matrix + i * columns + j) = atof(args[i * columns + j]);
-          printf("%f ",*(matrix + i * columns + j));
-      }printf("\n");
-    }
-    printf("\n\n");
-
+    //Creates averged matrix
     matavg(matrix, matrix_avrg, rows, columns);
 
+    printf("Input: \n");
+    for (int i = 0; i < rows; i++) {
+      for(int j = 0; j < columns; j++){
+        printf("%.2f ", *(matrix + i * columns + j));
+      }printf("\n");
+    }
+
+    printf("Output: \n");
+    for (int i = 0; i < rows-2; i++) {
+      for(int j = 0; j < columns-2; j++){
+        printf("%.2f ", *(matrix_avrg + i * (columns - 2) + j));
+      }printf("\n");
+    }
 }
